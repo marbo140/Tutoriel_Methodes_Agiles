cba8aea1b55fda9484fa992916464f6d7f4b91b0
marbo140 <55984629+marbo140@users.noreply.github.com>
2020-11-26 08:56:36 +0100

    Add files via upload

---
+++ Tutoriel/Prix.java
@@ -0,0 +1,24 @@
+
+/**
+ * Décrivez votre classe Prix ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Prix
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private int valeur = 0;
+    private String unite = "euro";
+
+    /**
+     * Constructeur d'objets de classe Prix
+     */
+    public Prix()
+    {
+        // initialisation des variables d'instance
+        this.valeur = 0;
+        this.unite = "euro";
+    }
+
+}
---
+++ Tutoriel/Vetement.java
@@ -0,0 +1,54 @@
+
+/**
+ */
+public class Vetement
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private String type = "Inconnu";
+    private String couleur = "Inconnu";
+    private Prix prix = null;
+
+    /**
+     * Constructeur d'objets de classe Vetement
+     */
+    public Vetement(String type, String couleur)
+    {
+        // initialisation des variables d'instance
+        this.type = type;
+        this.couleur = couleur;
+    }
+    
+    public String getType(){
+        return this.type;
+    }
+    
+    public void setType(String type){
+        this.type = type;
+    }
+    
+    public String getCouleur(){
+        return this.couleur;
+    }
+    
+    public void setCouleur(String couleur){
+        this.couleur = couleur;
+    }
+    
+    public void setPrix(Prix prix){
+        this.prix = prix;
+    }
+
+    /**
+     * Vérifier les attributs du vetement et lui ajoute un prix
+     * 
+     * @param type le type
+     * @param couleur la couleur
+     * @param prix une instance de la classe Prix
+     * return true si le vetement à le même type et la même couleur, false sinon
+     */
+    public boolean verifierVetement(String type, String couleur, Prix prix)
+    {
+        this.setPrix(prix);
+        return this.type == type && this.couleur == couleur;
+    }
+}
